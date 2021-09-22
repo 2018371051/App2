f3da645b8ea5797c82bcecab2b5e162d0d2bd84c
Consuelo Barrios Rodríguez <2018371051@uteq.edu.mx>
2021-09-21 22:50:26 -0500

    Agregar archivos de proyecto.

---
+++ App2/App2/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace App2.ViewModel
+{
+    class MainPageViewModel : ViewModelBase
+    {
+        private string _nombre;
+
+        public MainPageViewModel()
+        {
+            _nombre = "Holi";   
+        }
+
+        public string Nombre 
+        { 
+            get => _nombre;
+            set {
+                if (string.Equals(_nombre, value)) return;
+                var data = _nombre.CompareTo(value);
+                _nombre = value;
+                RaisedPropertyChanged(nameof(Nombre));
+                RaisedPropertyChanged(nameof(SinUpdate));
+            }
+        }
+        public string SinUpdate
+        {
+            get => _nombre;
+        }
+    }
+
+}
---
+++ App2/App2/ViewModel/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace App2.ViewModel
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisedPropertyChanged(string nombrePropiedad)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nombrePropiedad));
+        }
+    }
+}
